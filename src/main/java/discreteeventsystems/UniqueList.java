dc00bb413fa862c72e5a1607c24959df1011e483
Lewis404 <birkslewis@gmail.com>
2019-07-25 22:37:21 +0100

    Updated addAll methods to remove compiler warning: 'Collection<capture of ? extends E>' may not contain objects of type 'E'

--- src/main/java/discreteeventsystems/UniqueList.java
+++ src/main/java/discreteeventsystems/UniqueList.java
@@ -2,37 +2,61 @@ package discreteeventsystems;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
-public class UniqueList<T> extends ArrayList<T> {
+/**
+ * An ArrayList with the set property of ensuring that the collection only contains unique
+ * elements.
+ *
+ * @param <E> Type of the elements maintained in this list
+ */
+public class UniqueList<E> extends ArrayList<E> {
 
 	@Override
-	public boolean add(T t) {
-		if (this.contains(t)) {
+	public boolean add(E e) {
+		if (this.contains(e)) {
 			return false;
 		}
 
-		return super.add(t);
+		return super.add(e);
 	}
 
 	@Override
-	public void add(int index, T element) {
+	public void add(int index, E element) {
 		if (!this.contains(element)) {
 			super.add(index, element);
 		}
 	}
 
 	@Override
-	public boolean addAll(Collection<? extends T> c) {
-		Collection<? extends T> difference = new ArrayList<>(c);
-		difference.removeAll(this); // remove duplicates
-		return super.addAll(difference);
+	public boolean addAll(Collection<? extends E> c) {
+		boolean               modified = false;
+		Iterator<? extends E> iterator = c.iterator();
+		int                   size     = c.size();
+
+		while (--size >= 0) {
+			modified |= add(iterator.next());
+		}
+
+		return modified;
 	}
 
 	@Override
-	public boolean addAll(int index, Collection<? extends T> c) {
-		Collection<? extends T> retained = new ArrayList<>(c);
-		retained.removeAll(this); // remove duplicates
-		return super.addAll(index, retained);
+	public boolean addAll(int index, Collection<? extends E> c) {
+		boolean               modified = false;
+		Iterator<? extends E> iterator = c.iterator();
+		int                   size     = c.size();
+
+		while (--size >= 0) {
+			int sizeBefore = this.size();
+			add(index, iterator.next());
+			if (this.size() != sizeBefore) {
+				modified = true;
+				index++;
+			}
+		}
+
+		return modified;
 	}
 
 }
